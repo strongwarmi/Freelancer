faad5b0c9ab87750a0030af0e406d8a56807842d
strongwarmi <elianasc10@hotmail.com>
2017-05-10 13:33:46 -0500

    carga portfolio

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
-/*
- * Archivo principal de JS
- */
+window.addEventListener('load',function(){
+var ImgPortafolio=[{url:"cabin.png"},
+                   {url:"cake.png"},
+                   {url:"circus.png"},
+                   {url:"game.png"},
+                   {url:"safe.png"},
+                   {url:"submarine.png"}];
+function CreaPortafolio(){
+  var portafolio = document.getElementById("portafolio");
+  ImgPortafolio.forEach(function(Imagen){
+    var directorio = "assets/images/portfolio/" + Imagen.url;
+    var img = document.createElement('img');
+    img.className="picture";
+    img.id= "img" + ImgPortafolio.indexOf(Imagen);
+    img.setAttribute("src",directorio);
+    portafolio.appendChild(img);
+
+  });//cierra forEach
+}
+CreaPortafolio();
+});
